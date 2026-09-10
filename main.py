8b27086db7118a190fa1dc1149422d7fdffebc8f
Muhammad-zohaib-Abbas <zohaib.mza@gmail.com>
2026-09-10 15:39:08 +0500

    Environment variables

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 name = "Zohaib"
 
 user = {
@@ -5,6 +7,20 @@ user = {
     "age": 33
 }
 
-print(f"Hello, {user.name}!")
-print(f"age, {user.age}!")
+print(f"Hello, {user['name']}!")
+print(f"age, {user['age']}!")
 print("Welcome to Python for AI")
+
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+app_name = os.getenv("APP_NAME")
+
+print(app_name)
+print(api_key)
+
+if not api_key:
+    print("Key not found")
+else:
+    print(f"API key: {api_key}")
